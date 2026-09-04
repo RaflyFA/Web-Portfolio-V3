9fc3cc6563f82872b9f0035cef8ffa0c5013b92d
RAFLY_FA\rafly <rfl.yfa@gmail.com>
2026-09-04 10:28:24 +0700

    refactor: remove react-icons from optimizePackageImports in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     formats: ['image/webp', 'image/avif'],
   },
   experimental: {
-    optimizePackageImports: ['lucide-react', 'react-icons'],
+    optimizePackageImports: ['lucide-react'],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
